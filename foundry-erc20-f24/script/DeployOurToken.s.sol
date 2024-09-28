5dccd45bb7c8cfff7a7cfeda17388bc543241819
tzafff <christzaf99@gmail.com>
2024-09-28 17:28:09 +0300

    Manual Token, OpenZeppelin Token

---
+++ foundry-erc20-f24/script/DeployOurToken.s.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+import {OurToken} from "../src/OurToken.sol";
+
+contract DeployOurToken is Script{
+
+    uint256 public constant INITIAL_SUPPLY = 100 ether;
+
+
+    function run() external {
+        vm.startBroadcast();
+        new OurToken(INITIAL_SUPPLY);
+        vm.stopBroadcast();
+    }
+
+}
---
+++ foundry-erc20-f24/src/OurToken.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.19;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+contract OurToken is ERC20{
+    constructor(uint256 initialSupply) ERC20("Our Token", "OT") {
+        _mint(msg.sender, initialSupply);
+    }
+}
