885ea7bb492cfb6b0350f2304fba45fcb7d40218
tzafff <christzaf99@gmail.com>
2024-10-09 00:25:29 +0300

    MerkleAirdrop token,Deploy, Test files

---
+++ merkle-airdrop/src/MerkleAirdrop.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import { MerkleProof } from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import { IERC20, SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import { EIP712 } from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
+import { SignatureChecker } from "@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol";
+import { ECDSA } from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import { MessageHashUtils } from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
+
+contract MerkleAirdrop {
+
+    using SafeERC20 for IERC20; // Prevent sending tokens to recipients who can’t receive
+
+    error MerkleAirdrop__InvalidProof();
+    error MerkleAirdrop__AlreadyClaimed();
+
+    address[] claimers;
+    bytes32 private immutable i_merkleRoot;
+    IERC20 private immutable i_airdropToken;
+    mapping ( address claimer => bool claimed) private s_hasClaimed;
+
+
+    event Claim(address account, uint256 amount);
+
+
+    /*//////////////////////////////////////////////////////////////
+                               FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+    constructor(bytes32 merkleRoot, IERC20 airdropToken) {
+        i_merkleRoot = merkleRoot;
+        i_airdropToken = airdropToken;        
+    }
+
+    function claim(address account, uint256 amount, bytes32[] calldata merkleProof) external {
+        if(s_hasClaimed[account]){
+            revert MerkleAirdrop__AlreadyClaimed();
+        }
+        // calculate using the account and the amount, the hash -> leaf node
+        bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(account, amount))));
+        if(!MerkleProof.verify(merkleProof, i_merkleRoot, leaf)){
+            revert MerkleAirdrop__InvalidProof();
+        }
+        s_hasClaimed[account] = true;
+        emit Claim(account, amount);
+        i_airdropToken.safeTransfer(account, amount);
+       
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                             VIEW AND PURE
+    //////////////////////////////////////////////////////////////*/
+    function getMerkleRoot() external view returns (bytes32) {
+        return i_merkleRoot;
+    }
+
+    function getAirdropToken() external view returns (IERC20) {
+        return i_airdropToken;
+    }
+}
