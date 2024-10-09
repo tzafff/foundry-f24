e586875b55b238e0fc5e80d86c41b2dc691a211c
tzafff <christzaf99@gmail.com>
2024-10-10 01:14:30 +0300

    Deploy,Sign,Claim on ZKSYNC SEPOLIA

--- merkle-airdrop/script/GenerateInput.s.sol
+++ merkle-airdrop/script/GenerateInput.s.sol
@@ -16,7 +16,7 @@ contract GenerateInput is Script {
     function run() public {
         types[0] = "address";
         types[1] = "uint";
-        whitelist[0] = "0x6CA6d1e2D5347Bfab1d91e883F1915560e09129D";
+        whitelist[0] = "0x8D85e590D1109CCc55e54A9e495b023E543b521E";
         whitelist[1] = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
         whitelist[2] = "0x2ea3970Ed82D5b30be821FAAD4a731D35964F7dd";
         whitelist[3] = "0xf6dBa02C01AF48Cf926579F77C9f874Ca640D91D";
